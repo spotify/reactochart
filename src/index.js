3ae927884e32d916df169dbcec944b0358a5fcaa
Dan <dand@Dans-MacBook-Pro.local>
2015-09-14 14:23:08 -0400

    refer to charts with relative path in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-export { default as V1LineChart } from 'v1/LineChart';
+export { default as V1LineChart } from './v1/LineChart';
 
-export { default as PieChart } from 'charts/PieChart';
+export { default as PieChart } from './charts/PieChart';
 
-export { default as XYPlot } from 'charts/XYPlot';
-export { default as LineChart } from 'charts/LineChart';
-export { default as BarChart } from 'charts/BarChart';
-export { default as ScatterPlot } from 'charts/ScatterPlot';
-export { default as Histogram } from 'charts/Histogram';
-export { default as KernelDensityEstimation } from 'charts/KernelDensityEstimation';
+export { default as XYPlot } from './charts/XYPlot';
+export { default as LineChart } from './charts/LineChart';
+export { default as BarChart } from './charts/BarChart';
+export { default as ScatterPlot } from './charts/ScatterPlot';
+export { default as Histogram } from './charts/Histogram';
+export { default as KernelDensityEstimation } from './charts/KernelDensityEstimation';
