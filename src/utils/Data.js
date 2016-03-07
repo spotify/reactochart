2900cdba0cf7916856edf2b455e9ff07fb45739a
Dan Delany <dand@spotify.com>
2016-03-07 18:59:19 -0500

    inferDataType and inferDatasetsType Data utils

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -26,3 +26,26 @@ export function datasetsFromPropsOrDescendants(props) {
   }
   return [];
 }
+
+export function inferDataType(data, accessor = _.identity) {
+  if(!_.isArray(data))
+    throw new Error('inferDataType expects a data array');
+  else if(_.every(data, d => _.isUndefined(accessor(d))))
+    return 'categorical'; // should this be allowed?
+  else if(_.every(data, d => _.isNumber(accessor(d)) || _.isUndefined(accessor(d))))
+    return 'number';
+  else if(_.every(data, d => _.isDate(accessor(d)) || _.isUndefined(accessor(d))))
+    return 'time';
+  else
+    return 'categorical';
+}
+
+export function inferDatasetsType(datasets, accessor = _.identity) {
+  if(!_.isArray(datasets))
+    throw new Error('inferDatasetsType expects a datasets array');
+
+  const types = datasets.map(data => inferDataType(data, accessor));
+  console.log(types)
+  const uniqTypes = _.uniq(types);
+  return (uniqTypes.length === 1) ? uniqTypes[0] : 'categorical';
+}
--- tests/spec/utils.Data.spec.js
+++ tests/spec/utils.Data.spec.js
@@ -5,7 +5,11 @@ import TestUtils from 'react-addons-test-utils';
 import {expect} from 'chai';
 
 
-import {datasetsFromPropsOrDescendants} from '../../src/utils/Data'
+import {
+  datasetsFromPropsOrDescendants,
+  inferDataType,
+  inferDatasetsType
+} from '../../src/utils/Data'
 
 describe('datasetsFromPropsOrDescendants', () => {
   it('returns props.datasets', () => {
@@ -22,7 +26,7 @@ describe('datasetsFromPropsOrDescendants', () => {
     expect(datasets[0]).to.equal(props.data);
     expect(datasets[0]).to.deep.equal(props.data);
   });
-  
+
   it('traverses children and combines their props.data & props.datasets', () => {
     class TestComponent extends React.Component {
       render() {
@@ -36,10 +40,55 @@ describe('datasetsFromPropsOrDescendants', () => {
         <TestComponent datasets={[[2, 3], [4, 5]]}/>
         <div><TestComponent data={[6, 7]}/></div>
       </div></TestComponent>;
+
     const rendered = TestUtils.renderIntoDocument(tree);
     const datasets = datasetsFromPropsOrDescendants(rendered.props);
-
     expect(datasets).to.be.an('array');
     expect(datasets).to.deep.equal([[0, 1], [2, 3], [4, 5], [6, 7]]);
   });
 });
+
+
+describe('inferDataType', () => {
+  it('returns `number` for dataset with all numbers', () => {
+    expect(inferDataType([-1, 0, 1.2, 4.5])).to.equal('number');
+  });
+
+  it('returns `time` for dataset with all Dates', () => {
+    expect(inferDataType([new Date(), new Date(2003, 2, 4)])).to.equal('time');
+  });
+
+  it('returns `categorical` for dataset with all strings', () => {
+    expect(inferDataType(['a', 'b', 'c'])).to.equal('categorical');
+  });
+
+  it('returns `categorical` for mixed types', () => {
+    expect(inferDataType([42, new Date()])).to.equal('categorical');
+  });
+
+  it('takes an accessor function for getting the data points', () => {
+    const getD = (d) => d.a;
+    expect(inferDataType([{a: 1}, {a: 1.3}], getD)).to.equal('number');
+    expect(inferDataType([{a: new Date()}, {a: new Date()}], getD)).to.equal('time');
+    expect(inferDataType([{a: 'b'}, {a: 'c'}], getD)).to.equal('categorical');
+  })
+});
+
+describe('inferDatasetsType', () => {
+  it('returns same as inferDataType if all datasets are same type', () => {
+    expect(inferDatasetsType([[-1, 0], [1.2, 4.5]])).to.equal('number');
+    expect(inferDatasetsType([[new Date()], [new Date(2003, 2, 4)]])).to.equal('time');
+    expect(inferDatasetsType([['a'], ['b', 'c']])).to.equal('categorical');
+    expect(inferDatasetsType([[42, new Date()]])).to.equal('categorical');
+    
+    const getD = (d) => d.a;
+    expect(inferDatasetsType([[{a: 1}, {a: 1.3}]], getD)).to.equal('number');
+    expect(inferDatasetsType([[{a: new Date()}, {a: new Date()}]], getD)).to.equal('time');
+    expect(inferDatasetsType([[{a: 'b'}, {a: 'c'}]], getD)).to.equal('categorical');
+  });
+
+  it('returns `categorical` if datasets are mixed types', () => {
+    expect(inferDatasetsType([[7], [new Date()]])).to.equal('categorical');
+  });
+});
+
