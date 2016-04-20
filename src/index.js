4db259817197dae9391232e1988b1ebfc7942734
Dan Delany <dand@spotify.com>
2016-04-20 03:33:19 -0400

    remove old AreaHeatmap

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export { default as MarkerLineChart } from './charts/MarkerLineChart';
 export { default as ScatterPlot } from './charts/ScatterPlot';
 export { default as Histogram } from './charts/Histogram';
 export { default as KernelDensityEstimation } from './charts/KernelDensityEstimation';
-export { default as AreaHeatmap } from './charts/AreaHeatmap';
+export { default as AreaHeatmap } from './AreaHeatmap';
 
 export { default as PieChart } from './charts/PieChart';
 export { default as TreeMap } from './charts/TreeMap';
