84eb6b38a03c27fa36b6ca280ae77ce4f3dbf92b
Dan Delany <dand@spotify.com>
2017-08-02 16:02:38 -0400

    upgrade to latest webpack/webpack-dev-server, remove react-hot-loader, streamline webpack setup

--- docs/src/main.js
+++ docs/src/main.js
@@ -1,15 +1,6 @@
 import '../styles/main.less'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import update from 'react-addons-update';
-import numeral from 'numeral';
-//_.extend(window, {Perf, numeral});
-
-// import {App} from './Examples'
 import {App} from './App';
 
-console.log('app', App);
-
 ReactDOM.render(<App />, document.getElementById('container'));
-
-//export default App;
--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,17 +1,14 @@
 var webpack = require('webpack');
-// var CleanPlugin = require('clean-webpack-plugin');
 var _ = require('lodash');
 var config = require('./webpack.config.base');
 
 config = _.merge(config, {
   plugins: config.plugins.concat([
-    // new CleanPlugin(['build']),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.DedupePlugin(),
     // new webpack.optimize.UglifyJsPlugin()
   ])
 });
