90c7b656e33d858705a3d34a44c05bc54ab99ab3
Kris Salvador <krissalvador27@gmail.com>
2018-05-31 16:19:29 -0400

    PR review + documentation

--- src/SankeyDiagram.js
+++ src/SankeyDiagram.js
@@ -916,11 +916,10 @@ export default class SankeyDiagram extends React.Component {
       nodes.forEach(n => {
         depthMapXPos[n.depth] = n.x0;
 
-        if (depthMapYPos[n.depth]) {
-          depthMapYPos[n.depth] = Math.min(n.y0, depthMapYPos[n.depth]);
-        } else {
-          depthMapYPos[n.depth] = n.y0;
-        }
+        // For the given depth, set the y equal to the highest positioned y value
+        depthMapYPos[n.depth]
+          ? (depthMapYPos[n.depth] = Math.min(n.y0, depthMapYPos[n.depth]))
+          : (depthMapYPos[n.depth] = n.y0);
       });
 
       return (
