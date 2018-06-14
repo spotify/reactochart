045bd40ef18bbd44b4fe22dcfc8b48fc29988b93
joshb <joshb@spotify.com>
2018-06-14 11:53:14 -0400

    feature(tests): add in more tests

--- tests/browser/spec/XAxis.spec.js
+++ tests/browser/spec/XAxis.spec.js
@@ -1,7 +1,15 @@
 import { expect } from "chai";
 import { mount } from "enzyme";
 import React from "react";
-import { LineChart, XAxis, XYPlot } from "src/index.js";
+import {
+  LineChart,
+  XAxis,
+  XAxisLabels,
+  XAxisTitle,
+  XGrid,
+  XTicks,
+  XYPlot
+} from "src/index.js";
 
 // XAxis tests must run in browser since XAxis uses measureText
 
@@ -50,5 +58,36 @@ describe("XAxis", () => {
 
     expect(niceXChart.props().xDomain).to.deep.equal([0, 10]);
     expect(niceXChart.props().yDomain).to.deep.equal([0.8, 9.7]);
+  });
+  it("renders every part of the xAxis", () => {
+    const props = {
+      width,
+      height,
+      scaleType: { x: "linear", y: "linear" },
+      margin: { top: 11, bottom: 22, left: 33, right: 44 }
+    };
+
+    const tree = (
+      <XYPlot {...props} xDomain={[0, 10]} yDomain={[0, 10]}>
+        <LineChart data={[[0, 0], [10, 10]]} x={d => d[0]} y={d => d[1]} />
+        <XAxis ticks={[-5, 0, 5]} />
+      </XYPlot>
+    );
+    const rendered = mount(tree);
+    const lineChart = rendered
+      .find(XAxis)
+      .childAt(4)
+      .props();
+    expect(rendered.find(XGrid).props().ticks).to.have.length(3);
+    expect(rendered.find(XAxisLabels)).to.have.length(1);
+    expect(rendered.find(XAxisTitle)).to.have.length(1);
+    expect(rendered.find(XTicks)).to.have.length(1);
+    expect(lineChart.className).to.equal(
+      "rct-chart-axis-line rct-chart-axis-line-x"
+    );
+    expect(lineChart.x1).to.be.a("number");
+    expect(lineChart.x2).to.be.a("number");
+    expect(lineChart.y1).to.be.a("number");
+    expect(lineChart.y2).to.be.a("number");
   });
 });
--- tests/browser/spec/YAxis.spec.js
+++ tests/browser/spec/YAxis.spec.js
@@ -1,11 +1,9 @@
 import { expect } from "chai";
 import { mount } from "enzyme";
 import React from "react";
-import { LineChart, XYPlot, YAxis } from "src/index.js";
+import { LineChart, XYPlot, YAxis, YAxisLabels, YAxisTitle, YGrid, YTicks } from "src/index.js";
 
-
-
-// XAxis tests must run in browser since XAxis uses measureText
+// YAxis tests must run in browser since YAxis uses measureText
 
 describe("YAxis", () => {
   const width = 500;
@@ -40,12 +38,47 @@ describe("YAxis", () => {
 
     const niceXChart = mount(
       <XYPlot {...props}>
-        <LineChart data={[[0.3, 0.8], [9.2, 9.7]]} x={d => d[0]} y={d => d[1]} />
+        <LineChart
+          data={[[0.3, 0.8], [9.2, 9.7]]}
+          x={d => d[0]}
+          y={d => d[1]}
+        />
         <YAxis nice={true} />
       </XYPlot>
     ).find(LineChart);
 
     expect(niceXChart.props().xDomain).to.deep.equal([0.3, 9.2]);
     expect(niceXChart.props().yDomain).to.deep.equal([0, 10]);
+  });
+  it("renders every part of the yAxis", () => {
+    const props = {
+      width,
+      height,
+      scaleType: { x: "linear", y: "linear" },
+      margin: { top: 11, bottom: 22, left: 33, right: 44 }
+    };
+
+    const tree = (
+      <XYPlot {...props} xDomain={[0, 10]} yDomain={[0, 10]}>
+        <LineChart data={[[0, 0], [10, 10]]} x={d => d[0]} y={d => d[1]} />
+        <YAxis ticks={[-5, 0, 5]} />
+      </XYPlot>
+    );
+    const rendered = mount(tree);
+    const lineChart = rendered
+      .find(YAxis)
+      .childAt(4)
+      .props();
+    expect(rendered.find(YGrid).props().ticks).to.have.length(3);
+    expect(rendered.find(YAxisLabels)).to.have.length(1);
+    expect(rendered.find(YAxisTitle)).to.have.length(1);
+    expect(rendered.find(YTicks)).to.have.length(1);
+    expect(lineChart.className).to.equal(
+      "rct-chart-axis-line rct-chart-axis-line-y"
+    );
+    expect(lineChart.x1).to.be.a("number");
+    expect(lineChart.x2).to.be.a("number");
+    expect(lineChart.y1).to.be.a("number");
+    expect(lineChart.y2).to.be.a("number");
   });
 });
