0e942e2359c0bad5f118b542dd983699a9e03ab0
Kris Salvador <krissalvador27@gmail.com>
2018-06-27 11:20:20 -0400

    Remove commented out code on webpack doc build

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,19 +1,17 @@
-var path = require('path');
-var webpack = require('webpack');
-var _ = require('lodash');
-var config = require('./webpack.config.base');
-var CleanPlugin = require('clean-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var _ = require("lodash");
+var config = require("./webpack.config.base");
+var CleanPlugin = require("clean-webpack-plugin");
 
 config = _.merge(config, {
   plugins: config.plugins.concat([
     new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
-    new CleanPlugin([path.join(__dirname, 'docs/build')])
-
-    // new webpack.optimize.UglifyJsPlugin()
+    new CleanPlugin([path.join(__dirname, "docs/build")])
   ])
 });
 
