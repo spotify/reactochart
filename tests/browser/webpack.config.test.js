782bc3ae0bebcfadc1e4f92476d3bd145a28c00f
Kris Salvador <krissalvador27@gmail.com>
2019-06-11 12:18:13 -0400

    Upgrade babel

--- tests/browser/webpack.config.test.js
+++ tests/browser/webpack.config.test.js
@@ -34,7 +34,7 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }]
+        use: "babel-loader"
       },
       {
         test: /\.less?$/,
