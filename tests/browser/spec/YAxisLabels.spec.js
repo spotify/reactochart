41a0c45b2482118b68af8a74b5325a4e2907bd28
install <install@users.noreply.github.com>
2020-02-21 16:09:04 -0500

    Run Prettier on Scripts and Tests

--- tests/browser/spec/YAxisLabels.spec.js
+++ tests/browser/spec/YAxisLabels.spec.js
@@ -1,35 +1,35 @@
-import chai, { expect } from "chai";
-import { mount } from "enzyme";
-import React from "react";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
-import { XYPlot, YAxisLabels } from "src/index.js";
+import chai, { expect } from 'chai';
+import { mount } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { XYPlot, YAxisLabels } from 'src/index.js';
 chai.use(sinonChai);
 
 // YAxisLabels tests must run in browser since YAxisLabels uses measureText
 
-describe("YAxisLabel", () => {
+describe('YAxisLabel', () => {
   const width = 500;
   const height = 300;
   const props = {
     width,
     height,
-    xScaleType: "linear",
-    yScaleType: "linear",
+    xScaleType: 'linear',
+    yScaleType: 'linear',
     marginTop: 11,
     marginBottom: 22,
     marginLeft: 33,
     marginRight: 44,
-    offset: 5
+    offset: 5,
   };
 
-  it("Check how many labels are created and where", () => {
-    const chartStyle = { marginBottom: "10px" };
+  it('Check how many labels are created and where', () => {
+    const chartStyle = { marginBottom: '10px' };
     const functions = {
       onMouseEnterLabel: sinon.spy(),
       onMouseMoveLabel: sinon.spy(),
       onMouseLeaveLabel: sinon.spy(),
-      onMouseClickLabel: sinon.spy()
+      onMouseClickLabel: sinon.spy(),
     };
     const tree = (
       <div>
@@ -49,42 +49,42 @@ describe("YAxisLabel", () => {
     const rendered = mount(tree).find(YAxisLabels);
     const first = rendered.first();
     // each tick is a g and there's a wrapper around all g's, hence the 10 and 6
-    expect(first.find("g")).to.have.length(10);
-    expect(rendered.at(1).find("g")).to.have.length(6);
+    expect(first.find('g')).to.have.length(10);
+    expect(rendered.at(1).find('g')).to.have.length(6);
 
-    expect(rendered.at(1).props().position).to.equal("right");
-    expect(first.props().position).to.equal("left");
+    expect(rendered.at(1).props().position).to.equal('right');
+    expect(first.props().position).to.equal('left');
 
-    const firstChild = first.find("g").at(2);
+    const firstChild = first.find('g').at(2);
 
     expect(first.props().onMouseEnterLabel).not.to.have.been.called;
-    firstChild.simulate("mouseenter");
+    firstChild.simulate('mouseenter');
     expect(first.props().onMouseEnterLabel).to.have.been.calledOnce;
 
     expect(first.props().onMouseMoveLabel).not.to.have.been.called;
-    firstChild.simulate("mousemove");
+    firstChild.simulate('mousemove');
     expect(first.props().onMouseMoveLabel).to.have.been.calledOnce;
 
     expect(first.props().onMouseLeaveLabel).not.to.have.been.called;
-    firstChild.simulate("mouseleave");
+    firstChild.simulate('mouseleave');
     expect(first.props().onMouseLeaveLabel).to.have.been.calledOnce;
 
     expect(first.props().onMouseClickLabel).not.to.have.been.called;
-    firstChild.simulate("click");
+    firstChild.simulate('click');
     expect(first.props().onMouseClickLabel).to.have.been.calledOnce;
   });
 
-  it("Renders labels with given format and styles", () => {
+  it('Renders labels with given format and styles', () => {
     const tree = (
       <XYPlot width={400} height={150} xDomain={[-20, 20]} yDomain={[-20, 20]}>
         <YAxisLabels
-          format={d => `${d  }%`}
+          format={d => `${d}%`}
           position="left"
           distance={2}
           tickCount={5}
           labelStyle={label => {
             return {
-              fill: label.text === "0%" ? "green" : "blue"
+              fill: label.text === '0%' ? 'green' : 'blue',
             };
           }}
         />
@@ -92,10 +92,10 @@ describe("YAxisLabel", () => {
     );
 
     const rendered = mount(tree).find(YAxisLabels);
-    const labelWrapper = rendered.first("g");
-    const labels = labelWrapper.children().find("text");
+    const labelWrapper = rendered.first('g');
+    const labels = labelWrapper.children().find('text');
 
-    const correctTickLabels = ["-20%", "-10%", "0%", "10%", "20%"];
+    const correctTickLabels = ['-20%', '-10%', '0%', '10%', '20%'];
 
     const renderedTickLabels = labels.map(label => {
       const instance = label.instance();
@@ -103,14 +103,14 @@ describe("YAxisLabel", () => {
       const expectedStyles = Object.assign(
         YAxisLabels.defaultProps.labelStyle,
         {
-          fill: textContent === "0%" ? "green" : "blue"
-        }
+          fill: textContent === '0%' ? 'green' : 'blue',
+        },
       );
 
       Object.keys(expectedStyles).forEach(styleKey => {
         // Parse to int if lineHeight
         const styleValue =
-          styleKey === "lineHeight"
+          styleKey === 'lineHeight'
             ? parseInt(instance.style[styleKey], 10)
             : instance.style[styleKey];
 
@@ -123,16 +123,16 @@ describe("YAxisLabel", () => {
     expect(renderedTickLabels).to.eql(correctTickLabels);
   });
 
-  it("Renders date labels given formats array", () => {
+  it('Renders date labels given formats array', () => {
     const tree = (
       <XYPlot
         width={400}
         height={150}
-        yDomain={[new Date("01/01/2015"), new Date("01/01/2019")]}
+        yDomain={[new Date('01/01/2015'), new Date('01/01/2019')]}
         xDomain={[-20, 20]}
       >
         <YAxisLabels
-          formats={["%B %d, %Y", "%m/%Y"]}
+          formats={['%B %d, %Y', '%m/%Y']}
           position="left"
           distance={2}
           tickCount={5}
@@ -141,18 +141,18 @@ describe("YAxisLabel", () => {
     );
 
     const rendered = mount(tree).find(YAxisLabels);
-    const labelWrapper = rendered.first("g");
-    const labels = labelWrapper.children().find("text");
+    const labelWrapper = rendered.first('g');
+    const labels = labelWrapper.children().find('text');
 
     // Logic should pick our first format "%B %d, %Y"
     // which would format the labels like so January 30, 2015
     // because YAxisLabels (rendered vertically) wouldn't have collisions
     const correctTickLabels = [
-      "January 01, 2015",
-      "January 01, 2016",
-      "January 01, 2017",
-      "January 01, 2018",
-      "January 01, 2019"
+      'January 01, 2015',
+      'January 01, 2016',
+      'January 01, 2017',
+      'January 01, 2018',
+      'January 01, 2019',
     ];
 
     const renderedTickLabels = labels.map(label => {
@@ -164,11 +164,11 @@ describe("YAxisLabel", () => {
     expect(renderedTickLabels).to.eql(correctTickLabels);
   });
 
-  it("Renders number labels given formats array", () => {
+  it('Renders number labels given formats array', () => {
     const tree = (
       <XYPlot width={400} height={150} xDomain={[-20, 20]} yDomain={[-1, 1]}>
         <YAxisLabels
-          formats={[".1%", ".0%"]}
+          formats={['.1%', '.0%']}
           position="left"
           distance={2}
           tickCount={5}
@@ -177,13 +177,13 @@ describe("YAxisLabel", () => {
     );
 
     const rendered = mount(tree).find(YAxisLabels);
-    const labelWrapper = rendered.first("g");
-    const labels = labelWrapper.children().find("text");
+    const labelWrapper = rendered.first('g');
+    const labels = labelWrapper.children().find('text');
 
     // Logic should pick our first format ".1%"
     // which would format the labels like so: 1.0%
     // because YAxisLabels (rendered vertically) wouldn't have collisions
-    const correctTickLabels = ["-100.0%", "-50.0%", "0.0%", "50.0%", "100.0%"];
+    const correctTickLabels = ['-100.0%', '-50.0%', '0.0%', '50.0%', '100.0%'];
 
     const renderedTickLabels = labels.map(label => {
       const instance = label.instance();
--- tests/jsdom/spec/utils.Margin.spec.js
+++ tests/jsdom/spec/utils.Margin.spec.js
@@ -1,16 +1,16 @@
-import { expect } from "chai";
+import { expect } from 'chai';
 import {
   innerHeight,
   innerRangeX,
   innerRangeY,
   innerWidth,
   prefixKeys,
-  sumMargins
-} from "../../../src/utils/Margin";
+  sumMargins,
+} from '../../../src/utils/Margin';
 
-describe("Scale utils", () => {
-  describe("innerWidth", () => {
-    it("returns inner width value, given outer width and a margin object", () => {
+describe('Scale utils', () => {
+  describe('innerWidth', () => {
+    it('returns inner width value, given outer width and a margin object', () => {
       expect(innerWidth(500)).to.equal(500);
       expect(innerWidth(500, {})).to.equal(500);
       expect(innerWidth(500, { top: 100, bottom: 50 })).to.equal(500);
@@ -19,8 +19,8 @@ describe("Scale utils", () => {
     });
   });
 
-  describe("innerHeight", () => {
-    it("returns inner height value, given outer height and a margin object", () => {
+  describe('innerHeight', () => {
+    it('returns inner height value, given outer height and a margin object', () => {
       expect(innerHeight(500)).to.equal(500);
       expect(innerHeight(500, {})).to.equal(500);
       expect(innerHeight(500, { left: 100, right: 50 })).to.equal(500);
@@ -29,69 +29,69 @@ describe("Scale utils", () => {
     });
   });
 
-  describe("innerRangeX", () => {
-    it("returns inner X-range array, given outer width and a margin object", () => {
+  describe('innerRangeX', () => {
+    it('returns inner X-range array, given outer width and a margin object', () => {
       expect(innerRangeX(500)).to.deep.equal([0, 500]);
       expect(innerRangeX(500, {})).to.deep.equal([0, 500]);
       expect(innerRangeX(500, { top: 100, bottom: 50 })).to.deep.equal([
         0,
-        500
+        500,
       ]);
       expect(innerRangeX(500, { left: 100, right: 50 })).to.deep.equal([
         100,
-        450
+        450,
       ]);
       expect(innerRangeX(10, { left: 100, right: 50 })).to.deep.equal([10, 10]);
       expect(innerRangeX(120, { left: 100, right: 50 })).to.deep.equal([
         100,
-        100
+        100,
       ]);
     });
   });
 
-  describe("innerRangeY", () => {
-    it("returns inner Y-range array, given outer width and a margin object", () => {
+  describe('innerRangeY', () => {
+    it('returns inner Y-range array, given outer width and a margin object', () => {
       expect(innerRangeY(500)).to.deep.equal([500, 0]);
       expect(innerRangeY(500, {})).to.deep.equal([500, 0]);
       expect(innerRangeY(500, { left: 100, right: 50 })).to.deep.equal([
         500,
-        0
+        0,
       ]);
       expect(innerRangeY(500, { top: 100, bottom: 50 })).to.deep.equal([
         450,
-        100
+        100,
       ]);
       expect(innerRangeY(10, { top: 100, bottom: 50 })).to.deep.equal([10, 10]);
       expect(innerRangeY(120, { top: 100, bottom: 50 })).to.deep.equal([
         100,
-        100
+        100,
       ]);
     });
   });
 
-  describe("prefixKeys", () => {
+  describe('prefixKeys', () => {
     const obj = { a: 1, b: 2, c: 3 };
-    const prefix = "woot";
+    const prefix = 'woot';
 
     expect(prefixKeys(obj, prefix)).to.eql({
       wootA: 1,
       wootB: 2,
-      wootC: 3
+      wootC: 3,
     });
   });
 
-  describe("sumMargins", () => {
+  describe('sumMargins', () => {
     const margins = [
       { marginBottom: 5, marginTop: 0, marginLeft: 0, marginRight: 0 },
       { marginTop: 0, marginBottom: 29, marginLeft: 0, marginRight: 0 },
-      { marginBottom: 17, marginLeft: 16, marginRight: 16, marginTop: 0 }
+      { marginBottom: 17, marginLeft: 16, marginRight: 16, marginTop: 0 },
     ];
-    const prefix = "margin";
+    const prefix = 'margin';
     expect(sumMargins(margins, prefix)).to.eql({
       marginTop: 0,
       marginBottom: 51,
       marginLeft: 16,
-      marginRight: 16
+      marginRight: 16,
     });
   });
 });
