537a351a89def496e05162ac57d9b53e371c5efc
Anita <acmei@users.noreply.github.com>
2020-06-18 09:55:05 -0400

    Generate docs and add tests for onClick handler (#258)
    * generate docs and add tests for onClick handler
    
    * update names of contributers

--- tests/jsdom/spec/Bar.spec.js
+++ tests/jsdom/spec/Bar.spec.js
@@ -1,16 +1,15 @@
-import React from "react";
-import * as d3 from "d3";
-import _ from "lodash";
-import chai from "chai";
-import { mount, shallow } from "enzyme";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
+import React from 'react';
+import * as d3 from 'd3';
+import chai from 'chai';
+import { mount, shallow } from 'enzyme';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
 chai.use(sinonChai);
 const { expect } = chai;
 
-import { Bar } from "../../../src/index.js";
+import { Bar } from '../../../src/index.js';
 
-describe("Bar", () => {
+describe('Bar', () => {
   let horizontalBarProps;
   let verticalBarProps;
 
@@ -18,16 +17,16 @@ describe("Bar", () => {
     verticalBarProps = {
       xScale: d3
         .scalePoint()
-        .domain(["a", "b", "c"])
+        .domain(['a', 'b', 'c'])
         .range([0, 100]),
       yScale: d3
         .scaleLinear()
         .domain([0, 1])
         .range([100, 0]),
-      x: "a",
+      x: 'a',
       y: 0,
       yEnd: 1,
-      thickness: 10
+      thickness: 10,
     };
 
     horizontalBarProps = {
@@ -37,46 +36,46 @@ describe("Bar", () => {
         .range([0, 100]),
       yScale: d3
         .scalePoint()
-        .domain(["a", "b", "c"])
+        .domain(['a', 'b', 'c'])
         .range([100, 0]),
       x: 0.2,
       xEnd: 0.9,
-      y: "c",
-      thickness: 12
+      y: 'c',
+      thickness: 12,
     };
   });
 
-  it("renders a basic vertical bar correctly", () => {
+  it('renders a basic vertical bar correctly', () => {
     const bar = shallow(
       <Bar
         xScale={d3
           .scalePoint()
-          .domain(["a", "b", "c"])
+          .domain(['a', 'b', 'c'])
           .range([0, 100])}
         yScale={d3
           .scaleLinear()
           .domain([0, 1])
           .range([100, 0])}
-        x={"b"}
+        x="b"
         y={0.25}
         yEnd={0.75}
         thickness={20}
-      />
+      />,
     );
 
-    const rect = bar.find("rect");
+    const rect = bar.find('rect');
     expect(rect).to.have.length(1);
 
     const rectProps = rect.props();
-    expect(rectProps.className).to.include("rct-chart-bar");
-    expect(rectProps.className).to.include("rct-chart-bar-vertical");
+    expect(rectProps.className).to.include('rct-chart-bar');
+    expect(rectProps.className).to.include('rct-chart-bar-vertical');
     expect(rectProps.x).to.equal(50 - 20 / 2);
     expect(rectProps.y).to.equal(25);
     expect(rectProps.height).to.equal(50);
     expect(rectProps.width).to.equal(20);
   });
 
-  it("renders a basic horizontal bar correctly", () => {
+  it('renders a basic horizontal bar correctly', () => {
     const bar = shallow(
       <Bar
         xScale={d3
@@ -85,61 +84,61 @@ describe("Bar", () => {
           .range([0, 100])}
         yScale={d3
           .scalePoint()
-          .domain(["a", "b", "c"])
+          .domain(['a', 'b', 'c'])
           .range([100, 0])}
         x={0.1}
         xEnd={0.7}
-        y={"b"}
+        y="b"
         thickness={20}
-      />
+      />,
     );
 
-    const rect = bar.find("rect");
+    const rect = bar.find('rect');
     expect(rect).to.have.length(1);
 
     const rectProps = rect.props();
-    expect(rectProps.className).to.include("rct-chart-bar");
-    expect(rectProps.className).to.include("rct-chart-bar-horizontal");
+    expect(rectProps.className).to.include('rct-chart-bar');
+    expect(rectProps.className).to.include('rct-chart-bar-horizontal');
     expect(rectProps.x).to.equal(10);
     expect(rectProps.y).to.equal(50 - 20 / 2);
     expect(rectProps.height).to.equal(20);
     expect(rectProps.width).to.equal(60);
   });
 
-  it("has a thickness prop which controls the thickness of the bar", () => {
+  it('has a thickness prop which controls the thickness of the bar', () => {
     // verticalBar
     const verticalBar = shallow(<Bar {...verticalBarProps} />);
-    expect(verticalBar.find("rect")).to.have.length(1);
-    expect(verticalBar.find("rect").props().width).to.equal(10);
+    expect(verticalBar.find('rect')).to.have.length(1);
+    expect(verticalBar.find('rect').props().width).to.equal(10);
 
     const thickVerticalBar = shallow(
-      <Bar {...verticalBarProps} thickness={40} />
+      <Bar {...verticalBarProps} thickness={40} />,
     );
-    expect(thickVerticalBar.find("rect")).to.have.length(1);
-    expect(thickVerticalBar.find("rect").props().width).to.equal(40);
+    expect(thickVerticalBar.find('rect')).to.have.length(1);
+    expect(thickVerticalBar.find('rect').props().width).to.equal(40);
 
     // horizontalBar
     const horizontalBar = shallow(<Bar {...horizontalBarProps} />);
-    expect(horizontalBar.find("rect")).to.have.length(1);
-    expect(horizontalBar.find("rect").props().height).to.equal(12);
+    expect(horizontalBar.find('rect')).to.have.length(1);
+    expect(horizontalBar.find('rect').props().height).to.equal(12);
 
     const thickHorizontalBar = shallow(
-      <Bar {...horizontalBarProps} thickness={56} />
+      <Bar {...horizontalBarProps} thickness={56} />,
     );
-    expect(thickHorizontalBar.find("rect")).to.have.length(1);
-    expect(thickHorizontalBar.find("rect").props().height).to.equal(56);
+    expect(thickHorizontalBar.find('rect')).to.have.length(1);
+    expect(thickHorizontalBar.find('rect').props().height).to.equal(56);
   });
 
   it("passes className and style props through to the bar's rectangle element", () => {
     const barProps = {
-      className: "foo-bar-test-class",
-      style: { fill: "thistle" }
+      className: 'foo-bar-test-class',
+      style: { fill: 'thistle' },
     };
 
     const verticalBar = shallow(<Bar {...verticalBarProps} {...barProps} />);
-    const rect = verticalBar.find("rect");
+    const rect = verticalBar.find('rect');
     expect(rect).to.have.length(1);
-    expect(rect.props().className).to.include("foo-bar-test-class");
+    expect(rect.props().className).to.include('foo-bar-test-class');
     expect(rect.props().style).to.deep.equal(barProps.style);
   });
 
@@ -147,30 +146,35 @@ describe("Bar", () => {
     const barProps = {
       onMouseMove: sinon.spy(),
       onMouseEnter: sinon.spy(),
-      onMouseLeave: sinon.spy()
+      onMouseLeave: sinon.spy(),
+      onClick: sinon.spy(),
     };
 
     const bar = mount(<Bar {...verticalBarProps} {...barProps} />);
-    const rect = bar.find("rect");
+    const rect = bar.find('rect');
 
     expect(barProps.onMouseEnter).not.to.have.been.called;
-    rect.simulate("mouseenter");
+    rect.simulate('mouseenter');
     expect(barProps.onMouseEnter).to.have.been.calledOnce;
 
     expect(barProps.onMouseMove).not.to.have.been.called;
-    rect.simulate("mousemove");
+    rect.simulate('mousemove');
     expect(barProps.onMouseMove).to.have.been.calledOnce;
 
     expect(barProps.onMouseLeave).not.to.have.been.called;
-    rect.simulate("mouseleave");
+    rect.simulate('mouseleave');
     expect(barProps.onMouseLeave).to.have.been.calledOnce;
+
+    expect(barProps.onClick).not.to.have.been.called;
+    rect.simulate('click');
+    expect(barProps.onClick).to.have.been.calledOnce;
   });
 
-  it("throws an error if x/y scale(s) are missing or invalid", () => {
-    const barProps = { x: "a", y: 0, yEnd: 1 };
+  it('throws an error if x/y scale(s) are missing or invalid', () => {
+    const barProps = { x: 'a', y: 0, yEnd: 1 };
     const xScale = d3
       .scalePoint()
-      .domain(["a", "b", "c"])
+      .domain(['a', 'b', 'c'])
       .range([0, 100]);
     const yScale = d3
       .scaleLinear()
@@ -191,18 +195,18 @@ describe("Bar", () => {
     }).not.to.throw(Error);
   });
 
-  it("throws an error if exactly ONE of xEnd OR yEnd are not provided", () => {
+  it('throws an error if exactly ONE of xEnd OR yEnd are not provided', () => {
     const barProps = {
       xScale: d3
         .scalePoint()
-        .domain(["a", "b", "c"])
+        .domain(['a', 'b', 'c'])
         .range([0, 100]),
       yScale: d3
         .scaleLinear()
         .domain([0, 1])
         .range([100, 0]),
-      x: "a",
-      y: 0
+      x: 'a',
+      y: 0,
     };
 
     // throw if neither xEnd or yEnd passed
@@ -211,62 +215,62 @@ describe("Bar", () => {
     }).to.throw(Error);
     // throw if both xEnd and yEnd passed
     expect(() => {
-      shallow(<Bar {...{ ...barProps, xEnd: "b", yEnd: 1 }} />);
+      shallow(<Bar {...{ ...barProps, xEnd: 'b', yEnd: 1 }} />);
     }).to.throw(Error);
     // OK if one or other is passed
     expect(() => {
-      shallow(<Bar {...{ ...barProps, xEnd: "b" }} />);
+      shallow(<Bar {...{ ...barProps, xEnd: 'b' }} />);
     }).not.to.throw(Error);
     expect(() => {
       shallow(<Bar {...{ ...barProps, yEnd: 1 }} />);
     }).not.to.throw(Error);
   });
 
-  it("displays the x values when showLabel is true", () => {
+  it('displays the x values when showLabel is true', () => {
     const horizontalBarWithText = {
       ...horizontalBarProps,
-      showLabel: true
+      showLabel: true,
     };
 
     const horizontalBar = shallow(<Bar {...horizontalBarWithText} />);
-    expect(horizontalBar.find("rect")).to.have.length(1);
-    expect(horizontalBar.find("rect").props().height).to.equal(12);
-    expect(horizontalBar.find("text")).to.have.length(1);
+    expect(horizontalBar.find('rect')).to.have.length(1);
+    expect(horizontalBar.find('rect').props().height).to.equal(12);
+    expect(horizontalBar.find('text')).to.have.length(1);
 
     const verticalBarWithText = {
       ...verticalBarProps,
-      showLabel: true
+      showLabel: true,
     };
 
     const verticalBar = shallow(<Bar {...verticalBarWithText} />);
-    expect(verticalBar.find("rect")).to.have.length(1);
-    expect(verticalBar.find("rect").props().width).to.equal(10);
-    expect(horizontalBar.find("text")).to.have.length(1);
+    expect(verticalBar.find('rect')).to.have.length(1);
+    expect(verticalBar.find('rect').props().width).to.equal(10);
+    expect(horizontalBar.find('text')).to.have.length(1);
   });
 
   it("passes labelClassName through to the bar's text element", () => {
     const verticalProps = {
       ...verticalBarProps,
-      labelClassName: "foo-bar-test-class",
-      showLabel: true
+      labelClassName: 'foo-bar-test-class',
+      showLabel: true,
     };
 
     const verticalBar = shallow(<Bar {...verticalProps} />);
-    const text = verticalBar.find("text");
+    const text = verticalBar.find('text');
     expect(text).to.have.length(1);
-    expect(text.props().className).to.include("foo-bar-test-class");
+    expect(text.props().className).to.include('foo-bar-test-class');
   });
 
   it("passes labelFormat through to the bar's text value", () => {
     const verticalProps = {
       ...verticalBarProps,
       showLabel: true,
-      labelFormat: d => `${d}%`
+      labelFormat: d => `${d}%`,
     };
 
     const verticalBar = shallow(<Bar {...verticalProps} />);
-    const text = verticalBar.find("text");
+    const text = verticalBar.find('text');
     expect(text).to.have.length(1);
-    expect(text.props().children).to.equal("1%");
+    expect(text.props().children).to.equal('1%');
   });
 });
--- tests/jsdom/spec/RangeBarChart.spec.js
+++ tests/jsdom/spec/RangeBarChart.spec.js
@@ -1,35 +1,36 @@
-import { expect } from "chai";
-import * as d3 from "d3";
-import { mount } from "enzyme";
-import React from "react";
-import sinon from "sinon";
-import { Bar, RangeBarChart } from "../../../src/index.js";
+import { expect } from 'chai';
+import * as d3 from 'd3';
+import { mount } from 'enzyme';
+import React from 'react';
+import sinon from 'sinon';
+import { Bar, RangeBarChart } from '../../../src/index.js';
 
-describe("RangeBarChart", () => {
+describe('RangeBarChart', () => {
   const props = {
     xScale: d3
       .scalePoint()
-      .domain(["a", "b", "c"])
+      .domain(['a', 'b', 'c'])
       .range([0, 100]),
     yScale: d3
       .scaleLinear()
       .domain([0, 1])
       .range([100, 0]),
-    data: [["a", [0.3, 0.5]], ["b", [0.6, 0.9]]],
+    data: [['a', [0.3, 0.5]], ['b', [0.6, 0.9]]],
     x: d => d[0],
     y: d => d[1][0],
     yEnd: d => d[1][1],
     barThickness: 10,
-    barClassName: "my-bar",
-    barStyle: { fill: "blue" },
+    barClassName: 'my-bar',
+    barStyle: { fill: 'blue' },
     onMouseEnterBar: sinon.spy(),
     onMouseMoveBar: sinon.spy(),
-    onMouseLeaveBar: sinon.spy()
+    onMouseLeaveBar: sinon.spy(),
+    onClick: sinon.spy(),
   };
 
-  it("passes props to Bar elements", () => {
+  it('passes props to Bar elements', () => {
     const chart = mount(<RangeBarChart {...props} />);
-    const bars = chart.find("rect");
+    const bars = chart.find('rect');
 
     bars.forEach(bar => {
       expect(bar.props().className).to.contain(props.barClassName);
@@ -37,13 +38,13 @@ describe("RangeBarChart", () => {
     });
   });
 
-  it("renders a bar chart with categorical X data & numerical Y data", () => {
+  it('renders a bar chart with categorical X data & numerical Y data', () => {
     // make simple bar chart with 3 datapoints to make sure it renders correctly
     // this is more of an integration test/sanity check;
     // most tests for render correctness are in RangeRect and Bar specs
     const chart = mount(<RangeBarChart {...props} />);
-    const group = chart.find("g");
-    const bars = chart.find("rect");
+    const group = chart.find('g');
+    const bars = chart.find('rect');
     expect(group).to.have.length(1);
     expect(bars).to.have.length(2);
 
@@ -58,18 +59,24 @@ describe("RangeBarChart", () => {
     expect(bars.at(1).props().height).to.equal(30);
   });
 
-  it("triggers event handlers", () => {
+  it('triggers event handlers', () => {
     const chart = mount(<RangeBarChart {...props} />);
     const bars = chart.find(Bar);
 
     expect(props.onMouseMoveBar).not.to.have.been.called;
-    bars.at(1).simulate("mousemove");
+    bars.at(1).simulate('mousemove');
     expect(props.onMouseMoveBar).to.have.been.called;
+
     expect(props.onMouseEnterBar).not.to.have.been.called;
-    bars.at(1).simulate("mouseenter");
+    bars.at(1).simulate('mouseenter');
     expect(props.onMouseEnterBar).to.have.been.called;
+
     expect(props.onMouseLeaveBar).not.to.have.been.called;
-    bars.at(1).simulate("mouseleave");
+    bars.at(1).simulate('mouseleave');
     expect(props.onMouseLeaveBar).to.have.been.called;
+
+    expect(props.onClick).not.to.have.been.called;
+    bars.at(1).simulate('click');
+    expect(props.onClick).to.have.been.called;
   });
 });
