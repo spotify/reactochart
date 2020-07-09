96aa1984dd36910450050634e3705503551cb227
Scott Sheffield <scottux@gmail.com>
2020-07-09 15:30:01 -0400

    add alignment prop for domain start and end label to keep within chart footprint (#264)
    * add alignment prop for domain start and end label to keep within chart footprint
    
    * updating a very rigidly defined unit test
    
    * fix range lookup to actually use range directly
    
    * Updated comment on noLabelOverhang
    
    * Updated comment on noLabelOverhang again
    
    Co-authored-by: Scott Sheffield <scottux@spotify.com>

--- tests/jsdom/spec/utils.Axis.spec.js
+++ tests/jsdom/spec/utils.Axis.spec.js
@@ -1,13 +1,13 @@
-import { expect } from "chai";
-import * as d3 from "d3";
-import _ from "lodash";
+import { expect } from 'chai';
+import * as d3 from 'd3';
+import _ from 'lodash';
 import {
   getAxisChildProps,
-  getMouseAxisOptions
-} from "../../../src/utils/Axis";
+  getMouseAxisOptions,
+} from '../../../src/utils/Axis';
 
-describe("Axis utils", () => {
-  it("getAxisChildProps", () => {
+describe('Axis utils', () => {
+  it('getAxisChildProps', () => {
     const axisProps = {
       width: 400,
       height: 250,
@@ -17,147 +17,148 @@ describe("Axis utils", () => {
       spacingBottom: 10,
       spacingLeft: 10,
       spacingRight: 10,
-      position: "top",
-      placement: "",
+      position: 'top',
+      placement: '',
       ticks: [10, 20, 30, 40, 50],
       tickCount: 5,
       tickLength: 8,
-      tickClassName: "my-ticks",
-      tickStyle: { stroke: "blue" },
-      title: "what a title",
+      tickClassName: 'my-ticks',
+      tickStyle: { stroke: 'blue' },
+      title: 'what a title',
       titleDistance: 12,
-      titleAlign: "center",
+      titleAlign: 'center',
       titleRotate: true,
-      titleStyle: { color: "blue" },
+      titleStyle: { color: 'blue' },
       labelDistance: 12,
-      labelClassName: "my-label",
-      labelStyle: { color: "brown" },
-      labelFormat: "0a",
-      labelFormats: ["0a"],
-      labels: ["what a label"],
-      gridLineClassName: "my-grid",
+      labelClassName: 'my-label',
+      labelStyle: { color: 'brown' },
+      labelFormat: '0a',
+      labelFormats: ['0a'],
+      labels: ['what a label'],
+      gridLineClassName: 'my-grid',
       labelOffset: 10,
-      gridLineStyle: { stroke: "blue" },
+      gridLineStyle: { stroke: 'blue' },
       onMouseEnterLabel: () => {},
       onMouseMoveLabel: () => {},
       onMouseLeaveLabel: () => {},
-      onMouseClickLabel: () => {}
+      onMouseClickLabel: () => {},
     };
 
     const {
       ticksProps,
       gridProps,
       labelsProps,
-      titleProps
+      titleProps,
     } = getAxisChildProps(axisProps);
 
     expect(ticksProps).to.eql(
       _.pick(axisProps, [
-        "width",
-        "height",
-        "xScale",
-        "yScale",
-        "ticks",
-        "tickCount",
-        "spacingTop",
-        "spacingBottom",
-        "spacingLeft",
-        "spacingRight",
-        "position",
-        "placement",
-        "tickLength",
-        "tickStyle",
-        "tickClassName"
-      ])
+        'width',
+        'height',
+        'xScale',
+        'yScale',
+        'ticks',
+        'tickCount',
+        'spacingTop',
+        'spacingBottom',
+        'spacingLeft',
+        'spacingRight',
+        'position',
+        'placement',
+        'tickLength',
+        'tickStyle',
+        'tickClassName',
+      ]),
     );
 
     expect(gridProps).to.eql(
       Object.assign(
         {},
         _.pick(axisProps, [
-          "width",
-          "height",
-          "xScale",
-          "yScale",
-          "ticks",
-          "tickCount",
-          "spacingTop",
-          "spacingBottom",
-          "spacingLeft",
-          "spacingRight"
+          'width',
+          'height',
+          'xScale',
+          'yScale',
+          'ticks',
+          'tickCount',
+          'spacingTop',
+          'spacingBottom',
+          'spacingLeft',
+          'spacingRight',
         ]),
         {
           lineClassName: axisProps.gridLineClassName,
-          lineStyle: axisProps.gridLineStyle
-        }
-      )
+          lineStyle: axisProps.gridLineStyle,
+        },
+      ),
     );
 
     expect(labelsProps).to.eql(
       Object.assign(
-        {},
+        { noLabelOverhang: undefined },
         _.pick(axisProps, [
-          "width",
-          "height",
-          "xScale",
-          "yScale",
-          "ticks",
-          "tickCount",
-          "spacingTop",
-          "spacingBottom",
-          "spacingLeft",
-          "spacingRight",
-          "position",
-          "placement",
-          "labels",
-          "labelClassName",
-          "labelStyle",
-          "onMouseEnterLabel",
-          "onMouseMoveLabel",
-          "onMouseLeaveLabel",
-          "onMouseClickLabel"
+          'width',
+          'height',
+          'xScale',
+          'yScale',
+          'ticks',
+          'tickCount',
+          'spacingTop',
+          'spacingBottom',
+          'spacingLeft',
+          'spacingRight',
+          'position',
+          'placement',
+          'labels',
+          'labelClassName',
+          'labelStyle',
+          'noLabelOverhang',
+          'onMouseEnterLabel',
+          'onMouseMoveLabel',
+          'onMouseLeaveLabel',
+          'onMouseClickLabel',
         ]),
         {
           distance: axisProps.labelDistance,
           format: axisProps.labelFormat,
           formats: axisProps.labelFormats,
-          offset: axisProps.labelOffset
-        }
-      )
+          offset: axisProps.labelOffset,
+        },
+      ),
     );
 
     expect(titleProps).to.eql(
       Object.assign(
         {},
         _.pick(axisProps, [
-          "width",
-          "height",
-          "position",
-          "placement",
-          "title",
-          "spacingTop",
-          "spacingBottom",
-          "spacingLeft",
-          "spacingRight"
+          'width',
+          'height',
+          'position',
+          'placement',
+          'title',
+          'spacingTop',
+          'spacingBottom',
+          'spacingLeft',
+          'spacingRight',
         ]),
         {
           style: axisProps.titleStyle,
           distance: axisProps.titleDistance,
           alignment: axisProps.titleAlign,
-          rotate: axisProps.titleRotate
-        }
-      )
+          rotate: axisProps.titleRotate,
+        },
+      ),
     );
   });
 
-  describe("getMouseAxisOptions", () => {
-    it("throws error on invalid axis type", () => {
+  describe('getMouseAxisOptions', () => {
+    it('throws error on invalid axis type', () => {
       expect(() => {
-        getMouseAxisOptions("z", {}, {});
+        getMouseAxisOptions('z', {}, {});
       }).to.throw(Error);
     });
 
-    it("returns valid mouse options for x axisType", () => {
+    it('returns valid mouse options for x axisType', () => {
       const mockEvent = {
         currentTarget: {
           getBoundingClientRect: () => {
@@ -165,29 +166,29 @@ describe("Axis utils", () => {
               top: 0,
               left: 0,
               height: 300,
-              width: 300
+              width: 300,
             };
-          }
+          },
         },
         clientX: 50,
-        clientY: 0
+        clientY: 0,
       };
 
       const scale = d3
         .scalePoint()
-        .domain(["a", "b", "c"])
+        .domain(['a', 'b', 'c'])
         .range([0, 100]);
 
-      expect(getMouseAxisOptions("x", mockEvent, scale)).to.eql({
+      expect(getMouseAxisOptions('x', mockEvent, scale)).to.eql({
         event: mockEvent,
         outerX: 50,
         outerY: 0,
         xScale: scale,
-        xValue: "b"
+        xValue: 'b',
       });
     });
 
-    it("returns valid mouse options for y axisType", () => {
+    it('returns valid mouse options for y axisType', () => {
       const mockEvent = {
         currentTarget: {
           getBoundingClientRect: () => {
@@ -195,25 +196,25 @@ describe("Axis utils", () => {
               top: 0,
               left: 0,
               height: 300,
-              width: 300
+              width: 300,
             };
-          }
+          },
         },
         clientX: 0,
-        clientY: 50
+        clientY: 50,
       };
 
       const scale = d3
         .scalePoint()
-        .domain(["a", "b", "c"])
+        .domain(['a', 'b', 'c'])
         .range([0, 100]);
 
-      expect(getMouseAxisOptions("y", mockEvent, scale)).to.eql({
+      expect(getMouseAxisOptions('y', mockEvent, scale)).to.eql({
         event: mockEvent,
         outerX: 0,
         outerY: 50,
         yScale: scale,
-        yValue: "b"
+        yValue: 'b',
       });
     });
   });
